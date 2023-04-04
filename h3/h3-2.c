b1f514e689dce636f19883f79e0d0f29bccc178e
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-05 02:48:49 +0300

    commit

--- h3/h3-2.c
+++ h3/h3-2.c
@@ -23,51 +23,6 @@ struct Node* newNode(char data) {
 
 
 
-Node* createTree(char exp[]) {
-    int len = strlen(exp);
-    Node* stack[len];
-    int top = -1;
-     for (int i = 0; i < len; i++) {
-            Node* node = (Node*) malloc(sizeof(Node));
-            node->data = exp[i];
-            node->left = NULL;
-            node->right = NULL;
-
-    if (isalnum(exp[i])) {
-                stack[++top] = node;
-            } else {
-                Node* right = stack[top--];
-                Node* left = stack[top--];
-
-                node->left = left;
-                node->right = right;
-                stack[++top] = node;
-            }
-        }
-
-    return stack[top];
-}
-
-void printTree(Node* node, int space) {
-    if (node == NULL) {
-        return;
-    }
-
-    space += 4;
-printTree(node->right, space);
-    printf("\n");
-
-    for (int i = 4; i < space; i++) {
-        printf(" ");
-    }
-
-    printf("%c\n", node->data);
-
-    printTree(node->left, space);
-}
-
-
-
 
 
 // Inorder traversi ile ağacı yazdırma fonksiyonu
