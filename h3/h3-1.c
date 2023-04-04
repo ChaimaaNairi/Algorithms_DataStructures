ad59351ad654cf438b98f44b0db3047b9ef994dc
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-05 02:19:57 +0300

    commit

--- h3/h3-1.c
+++ h3/h3-1.c
@@ -6,3 +6,16 @@ typedef struct node {
     struct node* left;
     struct node* right;
 } Node;
+
+bool isTree(Node* node) {
+    // Eğer node NULL ise ağaçtır.
+    if (node == NULL) {
+        return true;
+    }
+    // Eğer node'un sadece bir çocuğu varsa, ağaç değildir.
+    if (node->left == NULL && node->right != NULL || node->left != NULL && node->right == NULL) {
+        return false;
+    }
+    // Sol ve sağ alt ağaçlar için kontrol yapılır.
+    return isTree(node->left) && isTree(node->right);
+}
