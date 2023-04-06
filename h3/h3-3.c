d34bc9b041dc685b0da5f75d2a552b180f432f82
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-06 03:29:40 +0300

    commit

--- h3/h3-3.c
+++ h3/h3-3.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+// Ağaç düğümü yapısı
+typedef struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+} Node;
+
+
