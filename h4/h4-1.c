385c24a12dfa454205eb0ec3220c50c26614f05c
ChaimaaN <chaimaanairi91@gmail.com>
2023-04-14 01:02:30 +0300

    commit

--- h4/h4-1.c
+++ h4/h4-1.c
@@ -1,234 +1,199 @@
 //Ö4-1: AVL ağacında silme operasyonu
 
-#include<stdio.h>
-#include<stdlib.h>
- 
-struct Node
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Tree{
     int key;
-    struct Node *left;
-    struct Node *right;
+    struct Tree *left,*right;
     int height;
-};
- 
-int max(int a, int b);
- 
-int height(struct Node *N)
+}tree;
+
+int height(tree *t)
 {
-    if (N == NULL)
+    if (t == NULL)
         return 0;
-    return N->height;
+    return t->height;
 }
- 
+
+int getBalance(tree *t)
+{
+    if(t==NULL)
+        return 0;
+    return height(t->left)-height(t->right);
+}
+
 int max(int a, int b)
 {
     return (a > b)? a : b;
 }
- 
-struct Node* newNode(int key)
+
+tree* newNode(int key)
 {
-    struct Node* node = (struct Node*)
-                        malloc(sizeof(struct Node));
+    tree* node = (tree*)malloc(sizeof(tree));
     node->key   = key;
     node->left   = NULL;
     node->right  = NULL;
     node->height = 1; 
     return(node);
 }
-
-struct Node *rightRotate(struct Node *y)
+tree* insert(tree* node, int key)
 {
-    struct Node *x = y->left;
-    struct Node *T2 = x->right;
- 
-    x->right = y;
-    y->left = T2;
- 
-    y->height = max(height(y->left), height(y->right))+1;
-    x->height = max(height(x->left), height(x->right))+1;
- 
-    return x;
-}
-
-struct Node *leftRotate(struct Node *x)
-{
-    struct Node *y = x->right;
-    struct Node *T2 = y->left;
- 
-    y->left = x;
-    x->right = T2;
- 
-    x->height = max(height(x->left), height(x->right))+1;
-    y->height = max(height(y->left), height(y->right))+1;
- 
-    return y;
-}
- 
-int getBalance(struct Node *N)
-{
-    if (N == NULL)
-        return 0;
-    return height(N->left) - height(N->right);
-}
- 
-struct Node* insert(struct Node* node, int key)
-{
+   
     if (node == NULL)
         return(newNode(key));
- 
+
     if (key < node->key)
         node->left  = insert(node->left, key);
-    else if (key > node->key)
+    else
         node->right = insert(node->right, key);
-    else 
-        return node;
- 
-    node->height = 1 + max(height(node->left),
-                           height(node->right));
- 
-    int balance = getBalance(node);
- 
-     if (balance > 1 && key < node->left->key)
-        return rightRotate(node);
- 
-    if (balance < -1 && key > node->right->key)
-        return leftRotate(node);
- 
-    if (balance > 1 && key > node->left->key)
-    {
-        node->left =  leftRotate(node->left);
-        return rightRotate(node);
-    }
- 
-    if (balance < -1 && key < node->right->key)
-    {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
- 
+
+    node->height = max(height(node->left), height(node->right)) + 1;
     return node;
 }
- 
-struct Node * minValueNode(struct Node* node)
-{
-    struct Node* current = node;
- 
-    while (current->left != NULL)
-        current = current->left;
- 
-    return current;
-}
- 
-struct Node* deleteNode(struct Node* root, int key)
-{
- 
-    if (root == NULL)
-        return root;
- 
-    if ( key < root->key )
-        root->left = deleteNode(root->left, key);
- 
- 
-    else if( key > root->key )
-        root->right = deleteNode(root->right, key);
 
-    else
+void print_preorder(tree * tTree)
+{
+    if(tTree)
     {
-        if( (root->left == NULL) || (root->right == NULL) )
+        printf("%d ",tTree->key);
+        print_preorder(tTree->left);
+        print_preorder(tTree->right);
+    }
+
+}
+
+void deleteTree(tree * tTree)
+{
+    if (tTree)
+    {
+        deleteTree(tTree->left);
+        deleteTree(tTree->right);
+        free(tTree);
+    }
+}
+
+tree* del(tree *tTree, int delItem) 
+{
+    if(!(tTree))
+        return tTree;
+    if(delItem < tTree->key)
+        tTree->left = del(tTree->left, delItem);
+    else 
+        if(delItem > tTree->key)
+            tTree->right = del(tTree->right, delItem); 
+        else 
         {
-            struct Node *temp = root->left ? root->left :
-                                             root->right;
- 
-            if (temp == NULL)
+            tree *oTree = tTree;
+            if((tTree->left) && (tTree->right))
             {
-                temp = root;
-                root = NULL;
+                tree *parent = tTree->right;
+                tTree = parent->left;
+                if (tTree)
+                {
+                    while(tTree->left)
+                    {
+                        parent = tTree;
+                        tTree = tTree->left;
+                    }
+                    parent->left = tTree->right;
+                    tTree->right = oTree->right;
+                }
+                else
+                    tTree = parent;
+                tTree->left = oTree->left;
             }
-            else 
-             *root = *temp;
-                            
-            free(temp);
+            else
+                if(tTree->left)
+                    tTree = tTree->left;
+                else
+                    tTree = tTree->right;
+            free(oTree);
         }
-        else
-        {
-
-            struct Node* temp = minValueNode(root->right);
- 
-            
-            root->key = temp->key;
- 
-         
-            root->right = deleteNode(root->right, temp->key);
-        }
-    }
-
-    if (root == NULL)
-      return root;
- 
-   
-    root->height = 1 + max(height(root->left),
-                           height(root->right));
- 
-
-    int balance = getBalance(root);
- 
-
-    if (balance > 1 && getBalance(root->left) >= 0)
-        return rightRotate(root);
- 
-  
-    if (balance > 1 && getBalance(root->left) < 0)
-    {
-        root->left =  leftRotate(root->left);
-        return rightRotate(root);
-    }
- 
-  
-    if (balance < -1 && getBalance(root->right) <= 0)
-        return leftRotate(root);
- 
- 
-    if (balance < -1 && getBalance(root->right) > 0)
-    {
-        root->right = rightRotate(root->right);
-        return leftRotate(root);
-    }
- 
-    return root;
+    return tTree;
 }
- 
-void preOrder(struct Node *root)
+
+tree* rotateLeft(tree* t)
+{ 
+    tree *y = t->right;
+    tree *T2 = y->left;
+
+    y->left = t;
+    t->right = T2;
+
+    t->height = max(height(t->left), height(t->right))+1;
+    y->height = max(height(y->left), height(y->right))+1;
+
+    return y;
+} 
+
+tree* rotateRight(tree* t)
+{ 
+    tree *x = t->left;
+    tree *T2 = x->right;
+
+    x->right = t;
+    t->left = T2;
+
+
+    t->height = max(height(t->left), height(t->right))+1;
+    x->height = max(height(x->left), height(x->right))+1;
+
+    return x;
+} 
+
+tree *balanceNode(tree *t)
 {
-    if(root != NULL)
+    if (!t) return t;
+    int balance = getBalance(t);
+    if (balance > 1 && getBalance(t->left) >= 0)  
+        return rotateRight(t);
+    if (balance < -1 && getBalance(t->right) <= 0) 
+        return rotateLeft(t);
+    if (balance > 1 && getBalance(t->left) < 0)    
     {
-        printf("%d ", root->key);
-        preOrder(root->left);
-        preOrder(root->right);
+        t->left = rotateLeft(t->left);
+        return rotateRight(t);
     }
+    if (balance < -1 && getBalance(t->right) > 0)  
+    {
+        t->right = rotateRight(t->right);
+        return rotateLeft(t);
+    }
+    return t;
 }
- 
-int main()
+
+
+int main(void)
 {
-  struct Node *root = NULL;
- 
-    root = insert(root, 9);
-    root = insert(root, 5);
-    root = insert(root, 10);
-    root = insert(root, 0);
-    root = insert(root, 6);
-    root = insert(root, 11);
-    root = insert(root, -1);
-    root = insert(root, 1);
-    root = insert(root, 2);
- 
-    printf("Olusturulan AVL'nin on siparis gecisi (Preorder traversal) "
-           "agac is \n");
-    preOrder(root);
- 
-    root = deleteNode(root, 10);
- 
-    printf("\n 10 silindikten sonra on siparis gecisi (Preorder traversal) \n");
-    preOrder(root);
- 
+    tree *Tree;
+    Tree = NULL;
+    int i, a[]={7,3,9,10,8,1,4,2,6,5};
+    for(i=0;i<10;i++) 
+        Tree = insert(Tree,a[i]);
+    printf("Pre Order Display\n");
+    print_preorder(Tree);
+
+    printf("\nBalanced agac\n");
+    Tree=balanceNode(Tree);
+    print_preorder(Tree);
+
+    while(1)
+    {
+        printf("\n Silmek istediginiz Bir ogeyi Girin: ");
+        scanf("%d",&i);
+        printf("\n");
+        Tree = del(Tree,i);
+        printf("Silme sonrasi \n");
+        print_preorder(Tree);
+        Tree = balanceNode(Tree);
+        printf("\n Balance sonrasi \n");
+        print_preorder(Tree);
+    }
+
+    printf("\n");
+
+    deleteTree(Tree);
+    printf("\n");
     return 0;
 }
--- h4/h4-2.c
+++ h4/h4-2.c
@@ -1,18 +1,19 @@
 //Ö4-2: B+ ağacında silme operasyonu
 
-
 #include<stdio.h>
 #include<stdlib.h>
 
+// A BTree node
 struct BTreeNode
 {
-    int *keys; 
-    int t;      
-    struct BTreeNode **C; 
-    int n;     
-    bool leaf; 
+    int *keys;  // An array of keys
+    int t;      // Minimum degree (defines the range for number of keys)
+    struct BTreeNode **C; // An array of child pointers
+    int n;     // Current number of keys
+    bool leaf; // Is true when node is leaf. Otherwise false
 };
 
+// A utility function to create a new BTree node
 struct BTreeNode *newNode(int t, bool leaf)
 {
     struct BTreeNode *node = new BTreeNode;
@@ -24,58 +25,82 @@ struct BTreeNode *newNode(int t, bool leaf)
     return node;
 }
 
+// A utility function to traverse all nodes in a subtree rooted with this node
 void traverse(struct BTreeNode *node)
 {
     int i;
     for (i = 0; i < node->n; i++)
     {
+        // If this is not leaf, then before printing key[i],
+        // traverse the subtree rooted with child C[i].
         if (node->leaf == false)
             traverse(node->C[i]);
         printf(" %d", node->keys[i]);
     }
 
+    // Print the subtree rooted with last child
     if (node->leaf == false)
         traverse(node->C[i]);
 }
 
+// A utility function to search a key in the subtree rooted with this node.
 struct BTreeNode *search(struct BTreeNode *node, int k)
 {
+    // Find the first key greater than or equal to k
     int i = 0;
     while (i < node->n && k > node->keys[i])
         i++;
 
+    // If the found key is equal to k, return this node
     if (node->keys[i] == k)
         return node;
 
+    // If key is not found here and this is a leaf node
     if (node->leaf == true)
         return NULL;
 
+    // Go to the appropriate child
     return search(node->C[i], k);
 }
 
+// A function to insert a new key in the subtree rooted with this node
+// The assumption is, the node must be non-full when this function is called
 void insertNonFull(struct BTreeNode *node, int k)
 {
+    // Initialize index as index of rightmost element
     int i = node->n-1;
 
+    // If this is a leaf node
     if (node->leaf == true)
     {
+        // The following loop does two things
+        // a) Finds the location of new key to be inserted
+        // b) Moves all greater keys to one place ahead
         while (i >= 0 && node->keys[i] > k)
         {
             node->keys[i+1] = node->keys[i];
             i--;
         }
 
+        // Insert the new key at found location
         node->keys[i+1] = k;
         node->n = node->n+1;
     }
-    else 
+    else // If this node is not leaf
     {
+        // Find the child which is going to have the new key
         while (i >= 0 && node->keys[i] > k)
             i--;
 
+        // See if the found child is full
         if (node->C[i+1]->n == 2*t-1)
         {
+            // If the child is full, then split it
             splitChild(node, i+1, node->C[i+1]);
+
+            // After split, the middle key of C[i] goes up and
+            // C[i] is splitted into two.  See which of the two
+            // is going to have the new key
             if (node->keys[i+1] < k)
                 i++;
         }
@@ -83,69 +108,96 @@ void insertNonFull(struct BTreeNode *node, int k)
     }
 }
 
+// A utility function to split the child y of this node
+// Note that y must be full when this function is called
 void splitChild(struct BTreeNode *node, int i, struct BTreeNode *y)
 {
+    // Create a new node which is going to store (t-1) keys
+    // of y
     struct BTreeNode *z = newNode(y->t, y->leaf);
     z->n = t - 1;
+
+    // Copy the last (t-1) keys of y to z
     for (int j = 0; j < t-1; j++)
         z->keys[j] = y->keys[j+t];
 
+    // Copy the last t children of y to z
     if (y->leaf == false)
     {
         for (int j = 0; j < t; j++)
             z->C[j] = y->C[j+t];
     }
 
+    // Reduce the number of keys in y
     y->n = t - 1;
 
+    // Since this node is going to have a new child,
+    // create space of new child
     for (int j = node->n; j >= i+1; j--)
         node->C[j+1] = node->C[j];
 
+    // Link the new child to this node
     node->C[i+1] = z;
 
+    // A key of y will move to this node. Find location of
+    // new key and move all greater keys one space ahead
     for (int j = node->n-1; j >= i; j--)
         node->keys[j+1] = node->keys[j];
 
+    // Copy the middle key of y to this node
     node->keys[i] = y->keys[t-1];
 
+    // Increment count of keys in this node
     node->n = node->n + 1;
 }
 
+// The main function that inserts a new key in this B-Tree
 void insert(struct BTreeNode *node, int k)
 {
+    // If tree is empty
     if (node == NULL)
     {
+        // Allocate memory for root
         node = newNode(t, true);
-        node->keys[0] = k;  
-        node->n = 1;  
+        node->keys[0] = k;  // Insert key
+        node->n = 1;  // Update number of keys in root
     }
-    else 
+    else // If tree is not empty
     {
+        // If root is full, then tree grows in height
         if (node->n == 2*t-1)
         {
+            // Allocate memory for new root
             struct BTreeNode *s = newNode(t, false);
 
+            // Make old root as child of new root
             s->C[0] = node;
 
+            // Split the old root and move 1 key to the new root
             splitChild(s, 0, node);
 
+            // New root has two children now.  Decide which of the
+            // two children is going to have new key
             int i = 0;
             if (s->keys[0] < k)
                 i++;
             insertNonFull(s->C[i], k);
 
+            // Change root
             node = s;
         }
-        else  
+        else  // If root is not full, call insertNonFull for root
             insertNonFull(node, k);
     }
 }
 
+// Driver program to test above functions
 
 int main()
 {
     struct BTreeNode *root = NULL;
 
+    // Let us create a B-Tree shown in the above figure
     insert(root, 10);
     insert(root, 20);
     insert(root, 5);
